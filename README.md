4b1b1f6e0c63065ef9aa3d385556a92dfb102264
Ollie Sims <oliver.sims@ge.com>
2018-12-14 15:48:52 +0000

    Add basic definition and reference support for Magik.

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ Magik language extension for VS Code.
 Adds the following features to VS Code:
 * Magik Syntax highlighting
 * Command `Magik Compile Method` (F7)
-* Command `Magik Compile File` (Ctrl + F7)
+* Command `Magik Compile File` (Ctrl+F7)
 * Command `Magik Compile Selection` (F8)
 * Command `Magik Goto` (F3) to jump to source. Click on a method name and invoke the command to jump to source or display method candidates at the Magik prompt.
 * Command `Magik Indent Method`
 * Adds _ before Magik keywords
 * Auto indenting of Magik code
+* Supports `Go to Definition` (F12) and `Peek Definition` (Alt+F12) in Magik. (Only searches in the current folder and open editors - use F3 to search all compiled code)
+* Supports `Find All References` (Shift+F12) and `List All References` (Shift+Alt+F12) in Magik. (Only searches in the current file - use Find in Folder to expand a search)
 
 (Use Ctrl + Shift + P to list available commands and type Magik)
 
@@ -33,16 +35,16 @@ I would recommend using these other extensions:
 
 ## Usage
 
-1. Open a repository folder in VS Code (**Note: Do this before opening a terminal**)
+1. Open a folder containing magik code in VS Code (**Note: Do this before opening a terminal**)
 
 2. Open a terminal in VS Code and start a magik session:
 e.g. S:\SW519\2018-11-30\core\bin\x86\runalias -a S:\SW519\2018-11-30\cambridge_db\config\gis_aliases cambridge_db_open
 
 3. Load the development procs:
 Load the file vscode_dev.magik at the Magik prompt.
-This will load a set of utility procs to support navigating Magik in VS Code.
+This will load a set of utility procs to support navigating and compiling Magik in VS Code.
 
-(vscode_dev.magik is supplied in this extension - I would recommend copying this to a convenient location to load after a session starts.)
+(vscode_dev.magik is supplied in this extension - I would recommend copying this to a convenient location to load after a session starts or load in .magik)
 
 The dev procs mfind() (aka mf()) and open_class() (aka oc()) will then be available at the Magik prompt.
 
@@ -62,8 +64,8 @@ The dev procs mfind() (aka mf()) and open_class() (aka oc()) will then be availa
 
 ## Known Issues
 
-* No grammar support
-* No syntax highlighting for msg files
+* Auto complete for keywords (without existing in current file)
+* No Magik snippets
 
 Please add issues here:
 https://github.build.ge.com/smallworld-sw5x/magik-vscode/issues
--- magik.js
+++ magik.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const vscode = require('vscode');
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
 const MAGIK_KEYWORDS = [
   'self',
@@ -32,6 +33,7 @@ const MAGIK_KEYWORDS = [
   'leave',
   'continue',
   'try',
+  'with',
   'when',
   'endtry',
   'throw',
@@ -132,10 +134,12 @@ const endAssignWords = [
   '_endprotect'
 ];
 
+const openFiles = [];
+
 
 function compileText(lines) {
   const path = 'C:/temp/temp_magik_vscode.magik';
-  const command = `load_file("${path}")\u000D`;
+  const command = 'vs_load()\u000D';
   const output = lines.join('\r\n');
 
   fs.writeFileSync(path, output);
@@ -161,22 +165,31 @@ function compileSelection() {
 
   if (!selection.empty) {
     const text = doc.getText(new vscode.Range(selection.start, selection.end));
-    const lines = ['#% text_encoding = iso8859_1', '_package sw', '$'];
+    const lines = ['#% text_encoding = iso8859_1', '_package sw', '$', '# Output:Loading selection ...'];
     lines.push(text)
     compileText(lines)
   }
 }
 
-function getLines(methodOnly) {
+function currentRegion(methodOnly, startRow) {
   const editor = vscode.window.activeTextEditor;
   const doc = editor.document;
-  const selection = editor.selection;
-  const startRow = selection.active.line;
+
+  if (!startRow) {
+    startRow = editor.selection.active.line;
+  }
+
   const lines = [];
+  const lineCount = doc.lineCount;
   let firstRow;
   let lastRow;
 
-  for (let row = startRow; row > 0; row--) {
+  if (!methodOnly) {
+    firstRow = 0;
+    lastRow = lineCount - 1;
+  }
+
+  for (let row = startRow; row > -1; row--) {
     const lineText = doc.lineAt(row).text;
     const lineTextTrimmed = lineText.trim();
 
@@ -185,7 +198,7 @@ function getLines(methodOnly) {
     if (lineTextTrimmed.startsWith('_method ') || lineTextTrimmed.includes(' _method ')) {
       firstRow = row;
       break;
-    } else if (lineTextTrimmed.startsWith('_endmethod') || lineTextTrimmed === '$') {
+    } else if (row < startRow - 1 && (lineTextTrimmed.startsWith('_endmethod') || lineTextTrimmed === '$')) {
       if (!methodOnly) {
         lines.shift();
         firstRow = row + 1;
@@ -194,18 +207,19 @@ function getLines(methodOnly) {
     }
   }
 
-  for (let row = startRow + 1; row < doc.lineCount + 1; row++) {
+  for (let row = startRow; row < lineCount; row++) {
     const lineText = doc.lineAt(row).text;
     const lineTextTrimmed = lineText.trim();
 
-    lines.push(lineText);
+    if (row !== startRow) {
+      lines.push(lineText);
+    }
 
     if (lineTextTrimmed.startsWith('_endmethod')) {
       lastRow = row;
       break;
     } else if (lineTextTrimmed.startsWith('_pragma') ||
-      lineTextTrimmed.startsWith('_method ') ||
-      lineTextTrimmed.includes(' _method ') ||
+      (row !== startRow && (lineTextTrimmed.startsWith('_method ') || lineTextTrimmed.includes(' _method '))) ||
       lineTextTrimmed === '$') {
       if (!methodOnly) {
         lines.pop();
@@ -215,18 +229,19 @@ function getLines(methodOnly) {
     }
   }
 
-  if (firstRow && lastRow) {
+  if (firstRow !== undefined && lastRow !== undefined) {
     return {lines, firstRow, lastRow};
   }
   return {};
 }
 
 function compileMethod() {
-  const lines = getLines(true).lines;
+  const lines = currentRegion(true).lines;
   if (!lines) return;
 
   const editor = vscode.window.activeTextEditor;
   const doc = editor.document;
+
   const parts = lines[0].split('.')
   const className = parts[0].split(' ').splice(-1)[0].trim();
   let methodName;
@@ -243,6 +258,7 @@ function compileMethod() {
     }
   }
 
+  lines.unshift(`# Output:Loading ${className}.${methodName} ...`);
   lines.unshift('$');
   lines.unshift('_package sw');
   lines.unshift('#% text_encoding = iso8859_1');
@@ -312,13 +328,13 @@ function goto() {
     }
   }
 
-  const command = `mgoto("^${methodText}$", "${classText}", ${inherit})\u000D`;
+  const command = `vs_goto("^${methodText}$", "${classText}", ${inherit})\u000D`;
   vscode.commands.executeCommand('workbench.action.terminal.focus', {});
   vscode.commands.executeCommand('workbench.action.terminal.sendSequence', {text: command});
 }
 
 async function indentMagik(currentRow) {
-  const { lines, firstRow } = getLines();
+  const { lines, firstRow } = currentRegion();
   if (!lines) return;
 
   const editor = vscode.window.activeTextEditor;
@@ -351,7 +367,6 @@ async function indentMagik(currentRow) {
     const indentText = (indent === 0) ? '' : new Array(indent + 1).join('\t');
 
     if (indentText !== text.slice(0, start)) {
-      //console.log('Update:', firstRow + row, text, indent);
       if (!currentRow || firstRow + row === currentRow) {
         const edit = new vscode.WorkspaceEdit();
         const range = new vscode.Range(firstRow + row, 0, firstRow + row, start);
@@ -442,6 +457,32 @@ function currentClassName(doc, pos) {
   }
 }
 
+function currentWord(doc, pos) {
+  const col = pos.character;
+  const text = doc.lineAt(pos.line).text;
+  let revText;
+  let start;
+  let end;
+
+  revText = text.slice(0, col);
+  revText = revText.split('').reverse().join('');
+  start = revText.search(INVALID_CHAR);
+  if (start === -1) return;
+
+  start = col - start;
+
+  end = text.slice(col).search(INVALID_CHAR);
+  if (end === -1) {
+    end = text.length;
+  } else {
+    end = col + end;
+  }
+
+  if (start !== end) {
+    return text.slice(start, end).trim();
+  }
+}
+
 function previousWord(doc, pos) {
   const line = doc.lineAt(pos.line);
   let text = line.text.slice(0, pos.character);
@@ -491,6 +532,15 @@ async function addUnderscore(doc, pos, ch) {
   const hashIndex = noStrings.indexOf('#');
   if (hashIndex !== -1 && hashIndex < textLength) return;
 
+  // Don't update in a string
+  let quotesCount = 0;
+  for (let i = 0; i < textLength; i++) {
+    if (text[i] === '"' && (i === 0 || text[i - 1] !== '%')) {
+      quotesCount++;
+    }
+  }
+  if (quotesCount % 2) return;
+
   const keywords = (ch === '.') ? MAGIK_VARIABLE_KEYWORDS : MAGIK_KEYWORDS;
   const keywordsLength = keywords.length;
 
@@ -525,11 +575,12 @@ async function addUnderscore(doc, pos, ch) {
 async function formatMagik(doc, pos, ch) {
   if (ch === '\n') {
     if (vscode.workspace.getConfiguration('magik-vscode').enableAutoIndentation) {
-      const lastCol = doc.lineAt(pos.line - 1).text.length;
-      const lastPos = new vscode.Position(pos.line - 1, lastCol);
+      const row = pos.line;
+      const lastCol = doc.lineAt(row - 1).text.length;
+      const lastPos = new vscode.Position(row - 1, lastCol);
       await addUnderscore(doc, lastPos, '');
-      await indentMagik(pos.line - 1);
-      await indentMagik(pos.line);
+      await indentMagik(row - 1);
+      await indentMagik(row);
     }
   } else {
     const edit = await addUnderscore(doc, pos, ch);
@@ -539,6 +590,155 @@ async function formatMagik(doc, pos, ch) {
   }
 }
 
+function getMagikFilesInDirectory(dir) {
+  const magikFiles = [];
+  const files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    if (path.extname(file) === '.magik') {
+      magikFiles.push(path.join(dir, file));
+    }
+  });
+  return magikFiles;
+}
+
+// TODO - assumes definitions are on one line!
+function findDefinition(fileName, word) {
+  const lines = fs.readFileSync(fileName).toString().split('\n'); // TODO replace this
+  const lineCount = lines.length;
+
+  const methodTest = new RegExp(`(^_method | _method ).+\\.\\s*(${word})`);
+  const defineTest = new RegExp(`\\.\\s*(define_slot_access|define_shared_constant|def_property|define_property|define_shared_variable)\\s*\\(\\s*:(${word})`);
+
+  for (let row = 0; row < lineCount; row++) {
+    const text = lines[row];
+    let index = text.search(methodTest);
+
+    if (index === -1) {
+      index = text.search(defineTest);
+    }
+    if (index !== -1) {
+      index = text.indexOf(word, index);
+      const range = new vscode.Range(row, index, row, index + word.length);
+      return new vscode.Location(vscode.Uri.file(fileName), range);
+    }
+  }
+}
+
+// TODO - only looking in current file, other open files and magik files in the current folder!
+function getMagikDefinition(doc, pos) {
+  const previous = previousWord(doc, pos);
+  if (previous !== '_self') return;
+
+  const current = currentWord(doc, pos);
+
+  // const methodTest = new RegExp(`(^_method | _method ).+\\.\\s*(${current})`);
+  // const defineTest = new RegExp(`\\.\\s*(define_slot_access|define_shared_constant|def_property|define_property|define_shared_variable)\\s*\\(\\s*:(${current})`);
+  // const lineCount = doc.lineCount;
+
+  // for (let row = 0; row < lineCount; row++) {
+  //   const line = doc.lineAt(row);
+  //   const text = line.text;
+  //   let index = text.search(methodTest);
+  //   if (index === -1) {
+  //     index = text.search(defineTest);
+  //   }
+
+  //   if (index !== -1) {
+  //     index = text.indexOf(current, index);
+  //     const range = new vscode.Range(row, index, row, index + current.length);
+  //     return new vscode.Location(doc.uri, range);
+  //   }
+  // }
+
+  const currentFileName = doc.fileName;
+  const currentDir = path.dirname(currentFileName);
+  const doneFileNames = [];
+  let loc;
+
+  // Check current file
+  loc = findDefinition(currentFileName, current);
+  if (loc) return loc;
+  doneFileNames.push(currentFileName);
+
+  // Check other open files
+  for (let i = 0; i < openFiles.length; i++) {
+    const fileName = openFiles[i];
+
+    if (!doneFileNames.includes(fileName) && path.extname(fileName) === '.magik') {
+      const loc = findDefinition(fileName, current);
+      if (loc) return loc;
+      doneFileNames.push(fileName);
+    }
+  }
+
+  // Check other magik files from the directory
+  const files = getMagikFilesInDirectory(currentDir);
+
+  for (let i = 0; i < files.length; i++) {
+    const fileName = files[i];
+
+    if (!doneFileNames.includes(fileName)) {
+      const loc = findDefinition(fileName, current);
+      if (loc) return loc;
+      doneFileNames.push(fileName);
+    }
+  }
+}
+
+// TODO - only looking in current file
+function getMagikReferences(doc, pos) {
+  const locs = [];
+  const current = currentWord(doc, pos);
+  if (current === '') return locs;
+
+  const lineCount = doc.lineCount;
+  const uri = doc.uri;
+
+  for (let row = 0; row < lineCount; row++) {
+    const text = doc.lineAt(row).text;
+    let index = text.indexOf(current);
+
+    if (index !== -1) {
+      const range = new vscode.Range(row, index, row, index + current.length);
+      const loc = new vscode.Location(uri, range);
+      locs.push(loc);
+    }
+  }
+
+  return locs;
+}
+
+// TODO - currently only looking for method definitions
+function getMagikSymbols(doc) {
+  const symbols = [];
+  const methodTest = new RegExp(`(^_method | _method ).+\\.\\s*.+`);
+  const methodType = vscode.SymbolKind.Method;
+  const lineCount = doc.lineCount;
+  const uri = doc.uri;
+
+  for (let row = 0; row < lineCount; row++) {
+    const text = doc.lineAt(row).text;
+
+    if (text.search(methodTest) !== -1) {
+      const parts = text.split('.')
+      const className = parts[0].split(' ').splice(-1)[0].trim();
+      let methodName;
+
+      if (parts.length > 1) {
+        methodName = parts[1].trim();
+        let index = methodName.search(INVALID_CHAR);
+        if (index === -1) index = methodName.length;
+        methodName = methodName.slice(0, index);
+        index = text.indexOf(methodName);
+        const range = new vscode.Range(row, index, row, index + methodName.length);
+        const sym = new vscode.SymbolInformation(methodName, methodType, range, uri, className);
+        symbols.push(sym);
+      }
+    }
+  }
+
+  return symbols;
+}
 
 function activate(context) {
   const config = [
@@ -564,6 +764,49 @@ function activate(context) {
         provideOnTypeFormattingEdits: formatMagik
       },
       ' ', '.', '\n'));
+
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      {
+        scheme: 'file',
+        language: 'magik'
+      },
+      {
+        provideDefinition: getMagikDefinition
+      }));
+
+  context.subscriptions.push(
+    vscode.languages.registerReferenceProvider(
+      {
+        scheme: 'file',
+        language: 'magik'
+      },
+      {
+        provideReferences: getMagikReferences
+      }));
+
+  context.subscriptions.push(
+    vscode.languages.registerDocumentSymbolProvider(
+      {
+        scheme: 'file',
+        language: 'magik'
+      },
+      {
+        provideDocumentSymbols: getMagikSymbols
+      }));
+
+  // No api for open editors
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor) {
+        const fileName = editor.document.fileName;
+        const index = openFiles.indexOf(fileName);
+        if (index === -1) {
+          openFiles.push(fileName);
+        } else {
+          openFiles.splice(index, 1);
+        }
+      }
+    }));
 }
 
 exports.activate = activate;
